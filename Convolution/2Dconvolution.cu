75e3874fb8cccf00e3ee67df6f2e3e7affb917f3
spig0126 <spig0126@gmail.com>
2022-05-12 13:26:37 +0900

    🚧 WIP: accurately saving tile elements on shared memory

--- Convolution/2Dconvolution.cu
+++ Convolution/2Dconvolution.cu
@@ -3,14 +3,15 @@
  *
  *       Filename:  main.cu
  *
- *    Description: 	Convolution
+ *    Description: 	2D-Convolution
  *
  *        Version:  1.0
- *        Created:  02/21/2022 10:07:38
+ *        Created:  2022/02/21 10:07:38
  *       Revision:  none
  *       Compiler:  nvcc
  *
  *         Author:  Yeonhee Jung
+ *   Organization:  Ewha Womans University
  *
  * =====================================================================================
  */
@@ -19,8 +20,8 @@
 #include "mkCuda.h"
 #include "mkClockMeasure.h"
 
-const int N_W = 256;
-const int N_H = 256;
+const int N_W = 512;
+const int N_H = 512;
 const int M_W = 5;
 const int M_H = 5;
 const int M_INTERVAL = M_W / 2;
@@ -30,15 +31,14 @@ const int MAX_ITER = 100;
 unsigned int N[N_W * N_H];
 unsigned int M[M_W * M_H];
 
-const int tbSize = N_W * N_H;
-const int threads = 16;
+const int threads = 32;
 const int TILE_SIZE = threads;
 
 unsigned int cpuOut[N_W * N_H];
 unsigned int gpuOut_2D_basic[N_W * N_H];
-unsigned int gpuOUt_2D_CM[N_W * N_H];
-unsigned int gpuOUt_2D_tiled[N_W * N_H];
-unsigned int gpuOUt_2D_tiled_internal[N_W * N_H];
+unsigned int gpuOut_2D_CM[N_W * N_H];
+unsigned int gpuOut_2D_tiled[N_W * N_H];
+unsigned int gpuOut_2D_tiled_internal[N_W * N_H];
 __constant__ unsigned int d_m_CM[M_W * M_H];
 
 void generateRandomValuesFor2D(unsigned int *input, const int row, const int col)
@@ -170,43 +170,180 @@ __global__ void gpu_2DConvolution_cm(const unsigned int *n, unsigned int *p, con
     p[row * width + col] = pVal;
 }
 
-__global__ void gpu_2DConvolution_tiled(const unsigned int *n, unsigned int *p, const int width, const int height, const int m_width, const int m_height, const int interval, const int TILE_SIZE)
+__global__ void gpu_2DConvolution_tiled(const unsigned int *n, unsigned int *p, const int width, const int height, const int m_width, const int m_height, const int interval, const int tile_size)
 {
-    __shared__ unsigned int N_cm[TILE_SIZE + m_width - 1][TILE_SIZE + m_height - 1];
-    int row = blockDim.y * blockIdx.y + threadIdx.y;
-    int col = blockDim.x * blockIdx.x + threadIdx.x;
+    __shared__ unsigned int SM[TILE_SIZE + M_W - 1][TILE_SIZE + M_H - 1];
+ 
     int tx = threadIdx.x;
     int ty = threadIdx.y;
-    int row_start_point = row - interval;
-    int col_start_point = col - interval;
+    //output indexes
+    int row_o = ty + blockIdx.y * tile_size;
+    int col_o = tx + blockIdx.x * tile_size;
+    //input indexes
+    int row_i = row_o - interval;
+    int col_i = col_o - interval;
 
-    if(row_start_point >= 0 && row_start_point < height && col_start_point >= 0 && col_start_point<width){
-        N_cm[ty][tx] = n[row_start_point * width + col_start_point];
+    //load tile elements
+    if(row_i >= 0 && row_i < height && col_i >= 0 && col_i<width){
+        SM[ty][tx] = n[row_i * width + col_i];
     }
     else{
-        N_cm[ty][tx] = 0;
+        SM[ty][tx] = 0;
     }
 
+    //wait till all tile elements are loaded
+    __syncthreads();
+
+    //output value
     int pVal = 0;
 
-    if(ty < )
-
-    for (int r_m = 0; r_m < m_height; r_m++)
-    {
-        for (int c_m = 0; c_m < m_width; c_m++)
+    //only compute if it is an output tile element
+    if(ty < tile_size && tx < tile_size){
+        for (int m_row = 0; m_row < m_height; m_row++)
         {
-            int currentRow = row_start_point + r_m;
-            int currentCol = col_start_point + c_m;
-            if (currentRow >= 0 && currentRow < height && currentCol >= 0 && currentCol < width)
+            for (int m_col = 0; m_col < m_width; m_col++)
             {
-                int nIndex = currentRow * width + currentCol;
-                pVal += n[nIndex] * d_m_CM[r_m * m_width + c_m];
+                pVal +=  d_m_CM[m_row*m_width + m_col] * SM[m_row + ty][m_col + tx];
+            }
+        }
+
+        //only save output value to output matrix when inside matrix bounds
+        if(row_o<width && col_o<height){
+            p[row_o*width + col_o] = pVal;
+        }
+    }
+}
+
+__global__ void gpu_2DConvolution_tiled_fixed(const unsigned int *n, unsigned int *p, const int width, const int height, const int m_width, const int m_height, const int interval, const int tile_size)
+{
+    __shared__ unsigned int SM[TILE_SIZE + M_W - 1][TILE_SIZE + M_H - 1];
+ 
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    //output indexes
+    int row_o = ty + blockIdx.y * tile_size;
+    int col_o = tx + blockIdx.x * tile_size;
+    //input indexes
+    int row_i = row_o - interval;
+    int col_i = col_o - interval;
+    //tile indexes
+    int row_t = ty + interval;
+    int col_t = tx + interval;
+
+    //LOAD TILE ELEMENTS
+    //original tile
+    SM[row_t][col_t] = n[row_o * width + col_o];
+    //outside border
+    if(ty == 0){    //top
+        for(int outside_row=0; outside_row < interval; outside_row++){
+            SM[outside_row][col_t] = row_i < 0? 0: n[(row_i + outside_row) * width + col_o];
+        }
+        if(tx == 0){    //top_left corner
+            for(int outside_row = 0; outside_row < interval; outside_row ++ ){
+                for(int outside_col = 0; outside_col < interval; outside_col ++ {
+                    SM[outside_row][outside_col] = row_i < 0 ? 0 : n[(row_i + outside_row) * width + col_i + outside_col];
+                }
+            }
+        }
+    }
+    else if(ty == tile_size - 1){ //bottom
+        for(int outside_row=0; outside_row < interval; outside_row++){
+            SM[row_t + outside_col][col_t] = row_o >= height? 0: n[(row_o + outside_row) * width + col_o];
+        }
+        if(tx == 0){    //bottom-right corner
+            for(int outside_row = 0; outside_row < interval; outside_row ++ ){
+                for(int outside_col = 0; outside_col < interval; outside_col ++ {
+                    SM[row_t + outside_row][col_t + outside_col] = row_o >= height? 0: n[(row_o +outside_row) * width + col_o + outside_col];
+                }
+            }
+        }
+    }
+    if(tx == 0){    //left
+        for(int outside_col=0; outside_col < interval; outside_col++){
+            SM[row_t][outside_col] = col_i < 0? n[row_o * width + col_i + outside_col];
+        }
+        if(tx == 0){    //bottom - left corner
+            for(int outside_row = 0; outside_row < interval; outside_row ++ ){
+                for(int outside_col = 0; outside_col < interval; outside_col ++ {
+                    SM[row_t + outside_row][outside_col] = row_i<0 ? 0 : n[(row_o + outside_row) * width + col_i + outside_col];
+                }
+            }
+        }
+    }
+    else if(tx == tile_size - 1){   //right
+        for(int outside_col=0; outside_col < interval; outside_col++){
+            SM[row_t][col_t + outside_col] = col_o >= height? n[row_o * width + col_o + outside_col];
+        }
+        if(ty == 0){    //top-right corner
+            for(int outside_row = 0; outside_row < interval; outside_row ++ ){
+                for(int outside_col = 0; outside_col < interval; outside_col ++ {
+                    SM[outside_row][col_t + outside_col] = row_o>=height? 0: n[(row_i +outside_row) * width + col_o + outside_col];
+                }
             }
         }
     }
 
-    p[row * width + col] = pVal;
+    //wait till all tile elements are loaded
+    __syncthreads();
+
+    //output value
+    int pVal = 0;
+
+    //only compute if it is an output tile element
+    if(ty < tile_size && tx < tile_size){
+        for (int m_row = 0; m_row < m_height; m_row++)
+        {
+            for (int m_col = 0; m_col < m_width; m_col++)
+            {
+                pVal +=  d_m_CM[m_row*m_width + m_col] * SM[ty - interval + m_row][tx - interval + m_col];
+            }
+        }
+
+        //only save output value to output matrix when inside matrix bounds
+        if(row_o<width && col_o<height){
+            p[row_o*width + col_o] = pVal;
+        }
+    }
+
+
 }
+
+// __global__ void gpu_2DConvolution_internal(const unsigned int *n, unsigned int *p, const int width, const int height, const int m_width, const int m_height, const int interval, const int tile_size)
+// {
+//     __shared__ unsigned int SM[TILE_SIZE][TILE_SIZE];
+
+//     int tx = threadIdx.x;
+//     int ty = threadIdx.y;
+//     int row = ty + blockIdx.y * tile_size;
+//     int col = tx + blockIdx.x * tile_size;
+//     int row_start_point = row - interval;
+//     int col_start_point = col - interval;
+
+//     //load tile 
+//     if(row < width && col < height){
+//         SM[ty][tx] = n[row * width + col];
+//     }
+
+//     //wait till all tile elements are loaded
+//     __syncthreads();
+
+//     //output value
+//     int pVal = 0;
+
+//     //only compute if it is an output tile element
+//     if(ty < tile_size && tx < tile_size){
+//         for(int m_row=0; m_row<m_height; m_row++){
+//             for(int m_col=0; m_col<m_width; m_col++){
+//                 int row_i = row_start_point + m_row
+//                 int col_i = col_start_point + m_col
+//                 if(row_i >= 0 && row_i < height && col_i >= 0 && col_i<width){
+//                     // if()
+//                     // pVal += 
+//                 }
+//             }
+//         }
+//     }
+// }
 int main()
 {
     srand((unsigned int)time(NULL));
@@ -214,7 +351,7 @@ int main()
     generateRandomValuesFor2D(M, M_W, M_H);
 
     // MK: GPU Memory
-    unsigned int *d_n, *d_m, *d_p;
+    unsigned int *d_n, *d_m, *d_p_basic, *d_p_CM, *d_p_tiled;
 
     int n_byteSize = N_W * N_H * sizeof(unsigned int);
     int m_byteSize = M_W * M_H * sizeof(unsigned int);
@@ -224,7 +361,11 @@ int main()
     checkCudaError(err);
     err = cudaMalloc((void **)&d_m, m_byteSize);
     checkCudaError(err);
-    err = cudaMalloc((void **)&d_p, n_byteSize);
+    err = cudaMalloc((void **)&d_p_basic, n_byteSize);
+    checkCudaError(err);
+    err = cudaMalloc((void **)&d_p_CM, n_byteSize);
+    checkCudaError(err);    
+    err = cudaMalloc((void **)&d_p_tiled, n_byteSize);
     checkCudaError(err);
 
     err = cudaMemcpy(d_n, N, n_byteSize, cudaMemcpyHostToDevice);
@@ -243,11 +384,12 @@ int main()
     // MK: Time Measurement
     mkClockMeasure *ckCpu = new mkClockMeasure("CPU CODE");
     ckCpu->clockReset();
-
     mkClockMeasure *ckGpu = new mkClockMeasure("GPU CODE");
     ckGpu->clockReset();
     mkClockMeasure *ckGpu_CM = new mkClockMeasure("GPU CODE - CM");
     ckGpu_CM->clockReset();
+    mkClockMeasure *ckGpu_tiled = new mkClockMeasure("GPU CODE - TILED");
+    ckGpu_tiled->clockReset();
 
     for (int i = 0; i < MAX_ITER; i++)
     {
@@ -256,42 +398,53 @@ int main()
         ckCpu->clockPause();
 
         ckGpu->clockResume();
-        gpu_2DConvolution_basic<<<gridSize, blockSize>>>(d_n, d_m, d_p, N_W, N_H, M_W, M_H, M_INTERVAL);
+        gpu_2DConvolution_basic<<<gridSize, blockSize>>>(d_n, d_m, d_p_basic, N_W, N_H, M_W, M_H, M_INTERVAL);
         err = cudaDeviceSynchronize();
         ckGpu->clockPause();
 
         ckGpu_CM->clockResume();
-        gpu_2DConvolution_cm<<<gridSize, blockSize>>>(d_n, d_p, N_W, N_H, M_W, M_H, M_INTERVAL);
+        gpu_2DConvolution_cm<<<gridSize, blockSize>>>(d_n, d_p_CM, N_W, N_H, M_W, M_H, M_INTERVAL);
         err = cudaDeviceSynchronize();
         ckGpu_CM->clockPause();
+
+        ckGpu_tiled->clockResume();
+        gpu_2DConvolution_tiled<<<gridSize, blockSize>>>(d_n, d_p_tiled, N_W, N_H, M_W, M_H, M_INTERVAL, TILE_SIZE);
+        err = cudaDeviceSynchronize();
+        ckGpu_tiled->clockPause();
+
         checkCudaError(err);
     }
 
-    err = cudaMemcpy(gpuOut_2D_basic, d_p, n_byteSize, cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(gpuOut_2D_basic, d_p_basic, n_byteSize, cudaMemcpyDeviceToHost);
+    checkCudaError(err);
+    err = cudaMemcpy(gpuOut_2D_CM, d_p_CM, n_byteSize, cudaMemcpyDeviceToHost);
+    checkCudaError(err);
+    err = cudaMemcpy(gpuOut_2D_tiled, d_p_tiled, n_byteSize, cudaMemcpyDeviceToHost);
     checkCudaError(err);
 
     cudaFree(d_n);
     cudaFree(d_m);
-    cudaFree(d_p);
+    cudaFree(d_p_basic);
+    cudaFree(d_p_CM);
+    cudaFree(d_p_tiled);
+
 
     if (compare2DMatrix(cpuOut, gpuOut_2D_basic, N_W, N_H))
     {
         printf("---------- [CPU] 2D convolution basic-------------\n");
         ckCpu->clockPrint();
-        printf("---------- [GPU] 2D convolution basic-------------\n");
+        printf("\n---------- [GPU] 2D convolution basic-------------\n");
         ckGpu->clockPrint();
-        printf("---------- [GPU] 2D convolution constant memory-------------\n");
+        printf("\n---------- [GPU] 2D convolution constant memory-------------\n");
         ckGpu_CM->clockPrint();
+        printf("\n---------- [GPU] 2D convolution tiled-------------\n");
+        ckGpu_tiled->clockPrint();
     }
     else
     {
         printf("ERROR: Two Matrices are not same\n");
     }
-    // print2DMask(M, M_W, M_H);
-    // print2DMatrix(N, N_W, N_H);
+
     // print2DMatrix(cpuOut, N_W, N_H);
     // print2DMatrix(gpuOut_2D_basic, N_W, N_H);
-
-    // print1DMatrixValue(cpuOut, N_WIDTH);
-    // print1DMatrixValue(gpuOut_1D_basic, N_WIDTH);
 }
