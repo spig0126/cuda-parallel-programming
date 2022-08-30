1ec5802a0d518a3e7748d627e3a48b9d4020b130
Yeonhee Jung <spig0126@ewhain.net>
2022-08-31 06:23:14 +0900

    💥 change c header file to cuda header file

--- FFT/comp.cuh
+++ FFT/comp.cuh
@@ -5,36 +5,30 @@ typedef struct Comp{
     double r, i;
 } Comp;
 
-Comp cal_euler(double x){
+__device__ __host__ Comp cal_euler(double x){
     Comp res;
     res.r = cos(x);
     res.i = sin(x);
     return res;
 }
 
-Comp comp_mult(Comp a, Comp b){
+__device__ __host__ Comp comp_mult(Comp a, Comp b){
     Comp res;
     res.r = a.r * b.r - a.i * b.i;
     res.i = a.r * b.i + a.i * b.r;
     return res;
 }
 
-Comp comp_add(Comp a, Comp b){
+__device__ __host__ Comp comp_add(Comp a, Comp b){
     Comp res;   
     res.r = a.r + b.r;
     res.i = a.i + b.i;
     return res;
 }
 
-Comp comp_sub(Comp a, Comp b){
+__device__ __host__ Comp comp_sub(Comp a, Comp b){
     Comp res;   
     res.r = a.r - b.r;
     res.i = a.i - b.i;
     return res;
-}
-
-Comp comp_cpy(Comp a, Comp b){
-    b.r = a.r;
-    b.i = a.i;
-    return a;
 }
